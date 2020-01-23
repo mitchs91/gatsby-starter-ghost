22d3f484ac4016619612c4fe348708e78c00de84
Mitchell Smith <mitchell.smith@xero.com>
2020-01-24 10:31:18 +1300

    reverting to initial state after some poor life choices on master

--- gatsby-config.js
+++ gatsby-config.js
@@ -179,12 +179,6 @@ module.exports = {
                 addUncaughtPages: true,
             },
         },
-        {
-            resolve: `gatsby-plugin-layout`,
-            options: {
-              component: require.resolve(`./src/layouts/index.js`),
-            },
-        }
         `gatsby-plugin-catch-links`,
         `gatsby-plugin-react-helmet`,
         `gatsby-plugin-force-trailing-slashes`,
